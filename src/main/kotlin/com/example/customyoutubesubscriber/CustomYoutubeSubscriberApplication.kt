8cb37c30ef7aec196f88a9a046ff4d46c172c8a0
김건호 <keno0403@estsecurity.com>
2023-03-03 12:36:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/customyoutubesubscriber/CustomYoutubeSubscriberApplication.kt
@@ -0,0 +1,11 @@
+package com.example.customyoutubesubscriber
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomYoutubeSubscriberApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomYoutubeSubscriberApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/customyoutubesubscriber/CustomYoutubeSubscriberApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.customyoutubesubscriber
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomYoutubeSubscriberApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
